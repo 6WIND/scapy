83b9d536609f320b4750936d087de357755c1245
Robin Jarry <robin.jarry@6wind.com>
2015-04-10 17:59:06 +0200

    contrib/mpls: add dynamic payload class detection
    Following the example from tcpdump source code:
    https://github.com/the-tcpdump-group/tcpdump/blob/master/print-mpls.c#L88

--- scapy/contrib/mpls.py
+++ scapy/contrib/mpls.py
@@ -5,6 +5,8 @@
 
 from scapy.packet import Packet, bind_layers
 from scapy.fields import BitField, ByteField
+from scapy.layers.inet import IP
+from scapy.layers.inet6 import IPv6
 from scapy.layers.l2 import Ether
 
 class MPLS(Packet): 
@@ -13,5 +15,19 @@ class MPLS(Packet):
                     BitField("cos", 0, 3), 
                     BitField("s", 1, 1), 
                     ByteField("ttl", 0)  ] 
+
+   def guess_payload_class(self, payload):
+       if self.label in (0, 3):
+           return IP
+       elif self.label == 2:
+           return IPv6
+       else:
+           first_byte = ord(str(payload)[0])
+           if first_byte >= 0x45 and first_byte <= 0x4f:
+               return IP
+           elif first_byte >= 0x60 and first_byte <= 0x6f:
+               return IPv6
+           else:
+               return self.default_payload_class(payload)
 
 bind_layers(Ether, MPLS, type=0x8847)

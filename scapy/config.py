cbcfb39cf9cdc34d25d581df83753cb75a23855e
Robin Jarry <robin.jarry@6wind.com>
2015-04-10 14:39:46 +0200

    6WIND version bump

--- scapy/config.py
+++ scapy/config.py
@@ -316,7 +316,7 @@ noenum    : holds list of enum fields for which conversion to string should NOT 
 AS_resolver: choose the AS resolver class to use
 extensions_paths: path or list of paths where extensions are to be looked for
 """
-    version = "2.2.0.6WIND"
+    version = "2.2.0.6WIND.1"
     session = ""
     interactive = False
     interactive_shell = ""
